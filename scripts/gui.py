7e95d4e1fca2031c0a638d1c05dca3f4f7a243b8
Bob <debortor@oregonstate.edu>
2019-01-12 14:03:03 -0800

    Added way to define buttons in a loop using the partial() function method of passing variables im iterating over.

--- scripts/gui.py
+++ scripts/gui.py
@@ -7,6 +7,7 @@ import pdb
 from gui_to_ros import pub_estop
 import rospy
 from std_msgs.msg import Int32
+from functools import partial
 
 
 # pdb.set_trace()
@@ -23,15 +24,16 @@ class CommandGUI:
         self.setupPubs()
 
         #setup the various sub-windows
-        self.setupCommandFrame(master, pubs = [self.estop_pubs])
+        self.setupCommandFrame(master)
 
     def setupPubs(self):
         self.estop_pubs = []
         #make custom publishers for each robot
         for robot_label in self.bot_labels:
+            print '/'+robot_label+'/e_stop'
             self.estop_pubs.append(rospy.Publisher('/'+robot_label+'/e_stop', Int32, queue_size=10))
     
-    def setupCommandFrame(self, master, pubs):
+    def setupCommandFrame(self, master):
         '''
         The frame for sending commands to the robots
         '''
@@ -45,9 +47,6 @@ class CommandGUI:
         for i, tab in enumerate(self.command_tabs):
             self.command_notebook.add(tab, text = self.bot_labels[i])
 
-        #publishers to be used
-        [estop_pub] = pubs
-
         
 
         #e-stop for every robot
@@ -56,16 +55,19 @@ class CommandGUI:
         for i in range(self.num_bots):
             self.estop_buts.append([])
         
-        #TODO: add window pop-up to this method to check fi they really want to hard e-stop
-        for i in range(self.num_bots):
-            self.estop_buts[i].append(Button(self.command_tabs[i], text="Hard e-stop", command = lambda: pub_estop(3, self.estop_pubs[i])))
-            self.estop_buts[i].append(Button(self.command_tabs[i], text="Soft e-stop", command = lambda: pub_estop(2, self.estop_pubs[i]))) 
-            self.estop_buts[i].append(Button(self.command_tabs[i], text="Pause", command = lambda: pub_estop(1, self.estop_pubs[i]))) 
-            self.estop_buts[i].append(Button(self.command_tabs[i], text="Resume", command = lambda: pub_estop(0, self.estop_pubs[i])))  
+        #TODO: add window pop-up to this method to check if they really want to hard e-stop
+        for i in range(self.num_bots): #note the use of partial() to send arguments via dynamic variables (vars we iterate over)!!
+            self.estop_buts[i].append(Button(self.command_tabs[i], text="Hard e-stop", command = partial(pub_estop,3, self.estop_pubs[i])))
+            self.estop_buts[i].append(Button(self.command_tabs[i], text="Soft e-stop", command = partial(pub_estop,3, self.estop_pubs[i]))) 
+            self.estop_buts[i].append(Button(self.command_tabs[i], text="Pause", command = partial(pub_estop,3, self.estop_pubs[i]))) 
+            self.estop_buts[i].append(Button(self.command_tabs[i], text="Resume", command = partial(pub_estop,3, self.estop_pubs[i]))) 
+
+        
 
         for button_set in self.estop_buts: #for the buttons of every robot
             for button in button_set: #for the button of a single robot
                 button.grid()
+
 
 
 
--- scripts/gui_to_ros.py
+++ scripts/gui_to_ros.py
@@ -4,13 +4,14 @@ from std_msgs.msg import Int32
 
 
 
-def pub_estop(number, pub):
+def pub_estop(number, pubs):
     '''
     Function to publish estop message (an int) after a button press
     '''
+    print pubs
     #TODO: add window pop-up to this method to check if they really want to hard e-stop
     print(number)
     msg_int = Int32()
     msg_int.data = number
-    pub.publish(msg_int)
+    pubs.publish(msg_int)
     
